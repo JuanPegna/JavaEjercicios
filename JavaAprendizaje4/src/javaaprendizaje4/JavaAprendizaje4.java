d293907dd375d9a0b7ddf1c3ffd20e9b5f3f4342
Juan Pena <juandapeva@gmail.com>
2023-04-17 21:50:20 -0500

    Ejercicio 4 terminado

--- JavaAprendizaje4/src/javaaprendizaje4/JavaAprendizaje4.java
+++ JavaAprendizaje4/src/javaaprendizaje4/JavaAprendizaje4.java
@@ -9,6 +9,7 @@ package javaaprendizaje4;
  *
  * @author juanp
  */
+import java.util.Scanner;
 public class JavaAprendizaje4 {
 
     /**
@@ -16,6 +17,13 @@ public class JavaAprendizaje4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner read =new Scanner(System.in);
+        float C,F;
+        System.out.println("Digite los grados Celsius para convertir");
+        C = read.nextFloat();
+        F=32+(9*(C/5));
+        System.out.println("la conversión de grados "+C+"C°, es en "+F+" F°");
+        
     }
     
 }
